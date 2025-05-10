27236627ec5ffa88f8a70120fe1dc97350e2b033
Linda Hrdinová <hrlin@seznam.cz>
2025-05-10 20:44:50 +0200

    components and header done

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import './header.css';
+
+export const Header = () => {
+  let navHidden = false;
+  return (
+    <header id="home">
+      <div className="header__content container">
+        <div className="site-logo"></div>
+
+        <div className="navigation">
+          <button className="nav-btn"></button>
+          {navHidden ? (
+            <nav className="inline-nav">
+              <a href="/">Hlavní stránka</a>
+            </nav>
+          ) : (
+            <nav className="rollout-nav nav-closed">
+              <a href="#home">domů</a>
+              <a href="#menu">menu</a>
+              <a href="#gallery">galerie</a>
+              <a href="#contact">kontakt</a>
+            </nav>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
